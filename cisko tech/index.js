fe7deea8e8e163c0df9ad89d345f388005bd703c
Civ60 <126390557+Civ60@users.noreply.github.com>
2025-05-24 13:32:12 +0000

    Update index.js

--- cisko tech/index.js
+++ cisko tech/index.js
@@ -4,6 +4,7 @@ window.onload = function () {
       selector: ".background"
     });
   };
+  
   const particles = Particles.init({
     selector: ".background",
     color: ["#03dac6", "#ff0266", "#000000"],
